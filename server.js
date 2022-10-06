4149747488e4ea5cbb6834bbb4e4d064aa801f43
Carlos Gámez <hardpx@gmail.com>
2022-10-06 13:32:13 -0500

    setup
    git commit --amend --date=“2022-mm-dd”
    git commit --amend --date=“2022-mm-dd”

--- server.js
+++ server.js
@@ -5,7 +5,28 @@ const PORT = 3000;
 
 app.use(cors());
 
-const pedals = {};
+const pedals = {
+  empress: {},
+  zvez: {},
+  boss: {},
+  strymon: {},
+  line6: {},
+  "tc electronix": {},
+  "electro-harmonix": {},
+};
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/:pedalName", (req, res) => {
+  const pedalName = req.params.pedalName.toLowerCase();
+  if (pedals[pedalName]) {
+    res.json(pedals[pedalName]);
+  } else {
+    res.json(pedal["not found"]);
+  }
+});
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`The server is running on PORT ${PORT}! YOU BETTER GO CATCH IT!`);
